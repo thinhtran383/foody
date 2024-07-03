5b9f1ee292ff2c116c6772275bbaead74e207d24
ThinhTran <thinhtran3803@gmail.com>
2024-07-03 18:56:04 +0700

    update endpoint for test

--- src/main/java/com/example/foodordering/controllers/HomeController.java
+++ src/main/java/com/example/foodordering/controllers/HomeController.java
@@ -1,9 +1,13 @@
 package com.example.foodordering.controllers;
 
+import com.example.foodordering.entities.Table;
 import com.example.foodordering.request.NotificationMessage;
 import com.example.foodordering.services.CloudinaryService;
 import com.example.foodordering.services.fcm.FirebaseMessagingService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import lombok.RequiredArgsConstructor;
@@ -34,23 +38,20 @@ public class HomeController {
     }
 
 
-    @PostMapping()
+    @PostMapping(consumes = { "multipart/form-data" })
     @OpenAPI30
-    public ResponseEntity<Map<String, String>> uploadImage(@RequestParam("image") MultipartFile file) {
+    public ResponseEntity<?> uploadImage(
+            @RequestPart("image") MultipartFile file,
+            @RequestPart("infor") String name
+    ) {
         Map<String, String> data = this.cloudinaryService.upload(file);
-        return new ResponseEntity<>(data, HttpStatus.OK);
+        System.out.println(name);
+        return ResponseEntity.ok().body(data);
     }
 
     @PostMapping("/notification")
-    public String sendNotificationByToken(){
-
-        NotificationMessage notificationMessage = NotificationMessage.builder()
-                .recipientToken("dcQnqTMuQ1e0TAm-yiKY8y:APA91bGKfyf2IJNrTIprc54ZMFn5VX_kGxJQ7Am65THkqyOo4M-SgziIpCvzUIYAGE1R7OqRTfjuJTflAsZbAJ0MhEYt_-G2ZukYTWxv3PtLaXG7IBOYMXt5Lz_quB9-M_5OMop4BEiY")
-                .title("thinh")
-                .body("body")
-                .image("image")
-                .build();
-        return firebaseMessagingService.sendNotificationByToken(notificationMessage);
+    public ResponseEntity<?> sendNotificationByToken(@RequestBody NotificationMessage notificationMessage){
+        return ResponseEntity.ok().body(firebaseMessagingService.sendNotificationByToken(notificationMessage));
     }
 
 
