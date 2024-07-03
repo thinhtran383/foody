c3290e4a76d1e269fbe3c1188c2c068762472c69
ThinhTran <thinhtran3803@gmail.com>
2024-07-03 18:49:43 +0700

    fix push notification

--- src/main/java/com/example/foodordering/configuration/FirebaseConfig.java
+++ src/main/java/com/example/foodordering/configuration/FirebaseConfig.java
@@ -4,24 +4,44 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Configuration
 public class FirebaseConfig {
+    @Value("${fcm.json.key.url}")
+    private String fcmJsonKeyUrl;
 
     @Bean
-    FirebaseMessaging firebaseMessaging() throws IOException {
-        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(
-                new ClassPathResource("food-ordering-1666f-firebase-adminsdk-pel1z-0fe88d6a94.json").getInputStream()
-        );
+    public FirebaseMessaging firebaseMessaging() throws IOException {
+        URL url = new URL(fcmJsonKeyUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new IOException("Failed to fetch JSON key file: " + connection.getResponseMessage());
+        }
+
+        InputStream serviceAccount = connection.getInputStream();
+//        GoogleCredentials googleCredentials = GoogleCredentials.fromStream( // from json file local
+//                new ClassPathResource("food-ordering-1666f-firebase-adminsdk-pel1z-d9ed44fb0a.json").getInputStream()
+//        );
+
+        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(serviceAccount);
 
         FirebaseOptions firebaseOptions = FirebaseOptions.builder()
                 .setCredentials(googleCredentials).build();
-        FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "my-app");
-        return FirebaseMessaging.getInstance(app);
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(firebaseOptions, "my-app");
+        }
+
+        return FirebaseMessaging.getInstance(FirebaseApp.getInstance("my-app"));
     }
 }
