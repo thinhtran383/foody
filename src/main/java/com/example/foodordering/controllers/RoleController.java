a97d76c785643f51dc783c2d11bbe0f880df7669
ThinhTran <thinhtran3803@gmail.com>
2024-07-17 02:42:47 +0700

    rename endpoint

--- src/main/java/com/example/foodordering/controllers/RoleController.java
+++ src/main/java/com/example/foodordering/controllers/RoleController.java
@@ -4,6 +4,7 @@ import com.example.foodordering.response.Response;
 import com.example.foodordering.services.RoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class RoleController {
     private final RoleService roleService;
 
-    @GetMapping
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<Response> getAll() {
         return ResponseEntity.ok().body(new Response("success", "Roles retrieved successfully", roleService.getAllRoles()));
     }
