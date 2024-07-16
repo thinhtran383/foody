42071c6e3b79d2603560745cb1982056f9df2a57
ThinhTran <thinhtran3803@gmail.com>
2024-07-17 02:44:03 +0700

    authorization

--- src/main/java/com/example/foodordering/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/foodordering/configuration/WebSecurityConfig.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,23 +23,29 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @RequiredArgsConstructor
 @EnableWebSecurity
 @EnableWebMvc
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
     private final JwtFilter jwtFilter;
     private final AuthenticationProvider authenticationProvider;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
 
     @Value("${api.v1.prefix}")
     String apiPrefix;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http.csrf(AbstractHttpConfigurer::disable);
         http.cors(AbstractHttpConfigurer::disable);
 
-        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
-        http.authorizeHttpRequests(request -> {
-                    request.requestMatchers(
+        http
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(request -> {
+                    request
+                            .requestMatchers(HttpMethod.GET,
+                                    // swagger
                                     "/swagger-ui/**",
                                     "/v3/api-docs/",
                                     "/v3/api-docs/**",
@@ -50,35 +58,93 @@ public class WebSecurityConfig {
                                     "/swagger-ui/**",
                                     "/swagger-ui.html",
 
-                                    // user
-                                    String.format("%s/roles", apiPrefix),
-                                    String.format("%s/users/**", apiPrefix),
-                                    String.format("%s/users/", apiPrefix),
 
-                                    // menu
-                                    String.format("%s/menu/**", apiPrefix),
-                                    String.format("%s/menu", apiPrefix),
-                                    "/new",
-
-                                    // category
-                                    String.format("%s/categories", apiPrefix),
-                                    String.format("%s/categories/**", apiPrefix),
-
-
-                                    // web-setting
+                                    // for landing page
                                     String.format("%s/web-settings", apiPrefix),
-                                    // table
-                                    String.format("%s/tables/**",apiPrefix)
+
+                                    // for menu
+                                    String.format("%s/menu/**", apiPrefix),
 
 
+                                    // for category
+                                    String.format("%s/categories/all", apiPrefix),
 
-                            ).permitAll()
+                                    // notification
+                                    String.format("%s/notification", apiPrefix)
+                            )
+                            .permitAll();
+
+                    request.requestMatchers(HttpMethod.POST,      // for user
+                            String.format("%s/users/login", apiPrefix),
+                            "/notification"
+
+                    ).permitAll();
+
+                    request.requestMatchers(
+                            String.format("%s/tables/**", apiPrefix),
+                            String.format("%s/orders/**", apiPrefix),
+                            String.format("%s/categories/**", apiPrefix),
+                            String.format("%s/menu/**", apiPrefix),
+                            String.format("%s/users/**", apiPrefix),
+                            String.format("%s/roles/**", apiPrefix),
+                            String.format("%s/web-settings/**", apiPrefix),
+                            String.format("%s/payment/**", apiPrefix),
+                            String.format("%s/notification/**", apiPrefix)
+
+                    ).hasAnyAuthority("ROLE_ADMIN")
                             .anyRequest()
                             .authenticated();
 
-
                 })
-                .authenticationProvider(authenticationProvider);
+                .authenticationProvider(authenticationProvider)
+                .exceptionHandling(ex -> ex.accessDeniedHandler(accessDeniedHandler));
+
+
+//        http.authorizeHttpRequests(request -> {
+//                    request.requestMatchers(
+//                                    "/swagger-ui/**",
+//                                    "/v3/api-docs/",
+//                                    "/v3/api-docs/**",
+//                                    "/api-docs",
+//                                    "/api-docs/**",
+//                                    "/swagger-resources",
+//                                    "/swagger-resources/**",
+//                                    "/configuration/ui",
+//                                    "/configuration/security",
+//                                    "/swagger-ui/**",
+//                                    "/swagger-ui.html",
+//
+//                                    // user
+//                                    String.format("%s/roles", apiPrefix),
+//                                    String.format("%s/users/**", apiPrefix),
+//                                    String.format("%s/users/", apiPrefix),
+//
+//                                    // menu
+//                                    String.format("%s/menu/**", apiPrefix),
+//                                    String.format("%s/menu", apiPrefix),
+//
+//
+//                                    // category
+//                                    String.format("%s/categories", apiPrefix),
+//                                    String.format("%s/categories/**", apiPrefix),
+//
+//
+//                                    // web-setting
+//                                    String.format("%s/web-settings", apiPrefix),
+//                                    // table
+//                                    String.format("%s/tables/**",apiPrefix)
+//
+////                                    "/test"
+//
+//
+//
+//                            ).permitAll()
+//                            .anyRequest()
+//                            .authenticated();
+//
+//
+//                })
+//                .authenticationProvider(authenticationProvider);
 
 
         return http.build();
