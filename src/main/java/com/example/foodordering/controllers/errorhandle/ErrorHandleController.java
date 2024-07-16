ba87143cfe2929d869dc4f954a374e9f40e152c7
ThinhTran <thinhtran3803@gmail.com>
2024-07-17 02:40:33 +0700

    add handle when endpoint not exist

--- src/main/java/com/example/foodordering/controllers/errorhandle/ErrorHandleController.java
+++ src/main/java/com/example/foodordering/controllers/errorhandle/ErrorHandleController.java
@@ -1,19 +1,22 @@
 package com.example.foodordering.controllers.errorhandle;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
-public class ErrorHandleController {
+public class ErrorHandleController implements ErrorController{
 
-    @GetMapping("/error/403")
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public String handle403() {
-        return "403"; // Trả về template 403.html trong thư mục templates
+    @RequestMapping("/error")
+    @ResponseStatus(HttpStatus.BAD_GATEWAY)
+    @ResponseBody
+    public String handleError(HttpServletRequest request) {
+        return "Resource not found";
     }
-
 
 }
