45377ef78ba819b5c5cf72a27fd326d94a9d6be1
ThinhTran <thinhtran3803@gmail.com>
2024-07-17 02:45:02 +0700

    add constraint

--- src/main/java/com/example/foodordering/dtos/NotificationMessageDTO.java
+++ src/main/java/com/example/foodordering/dtos/NotificationMessageDTO.java
@@ -1,6 +1,8 @@
 package com.example.foodordering.dtos;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,11 +11,19 @@ import java.util.Map;
 @Data
 @Builder
 public class NotificationMessageDTO {
+    @Hidden
     private String recipientToken;
+
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @NotBlank(message = "Body is required")
     private String body;
+
+
     private String image;
 
     @Schema(hidden = true)
+    @Hidden
     private Map<String, String> data;
 }
