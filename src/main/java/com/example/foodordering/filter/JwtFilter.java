e5a7bf5354a2e7e3ff7ceabc5e915708a8c0bc6f
ThinhTran <thinhtran3803@gmail.com>
2024-08-05 11:04:40 +0700

    test sign

--- src/main/java/com/example/foodordering/filter/JwtFilter.java
+++ src/main/java/com/example/foodordering/filter/JwtFilter.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
